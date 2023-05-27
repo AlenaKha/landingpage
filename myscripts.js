1659e23baac79106494baf4e65bdaa135ad87d87
AlenaKha <131781907+AlenaKha@users.noreply.github.com>
2023-05-27 18:36:00 +0100

    Create myscripts.js

---
+++ myscripts.js
@@ -0,0 +1,3 @@
+const image=document.getElementById("logotype");
+logotype.addEventListener("mouseout", function() {logotype.width="300" });
+logotype.addEventListener("mouseover", function() {logotype.width="400" });
